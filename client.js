75b19815e8cb7017225baf90f95fbe48fd067c02
navid bahrami <navidbahrami1996@gmail.com>
2024-11-22 16:41:30 +0330

    client

--- client.js
+++ client.js
@@ -1,14 +1,11 @@
-const teacherSocket = io('http://localhost:3000/teacher');
-teacherSocket.on('connect' , data => {
-    teacherSocket.emit('teacherClient' , 'hello server i am client') //emit for send data and on for reading data
-    teacherSocket.on('teacherServer' , data => {
-        console.log(data);
+const socket = io('http://localhost:3000');
+socket.on('connect' , data => {
+    const sendBtn = document.getElementById('sendBtn')
+    sendBtn.addEventListener('click' , (e) => {
+    const textBox = document.getElementById('text');
+    const message = textBox.value;
+    if(!message) return alert('text box can not be empty')
+        socket.emit('clientMessage' , message)
+    textBox.value = ''
     })
-}) 
-const studentSocket = io('http://localhost:3000/student');
-studentSocket.on('connect' , data => {
-    studentSocket.emit('studentClient' , 'hello server i am client') //emit for send data and on for reading data
-    studentSocket.on('studentServer' , data => {
-        console.log(data);
-    })
-}) 
+})
