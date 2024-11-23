67c63b799a9add86fe2f1287e33e4670f9adc011
navid bahrami <navidbahrami1996@gmail.com>
2024-11-23 18:57:01 +0330

    client

--- client.js
+++ client.js
@@ -1,3 +1,16 @@
+function initializeChatBox(){
+    const localStorageValue = localStorage.getItem('messages')
+    const messages = (localStorageValue? localStorageValue.split('#'): [] ).map(item => {
+        if(item) return item
+    })
+    messages.forEach(item => {
+        const paragraphElement = document.createElement('p')
+        paragraphElement.innerText = item;
+        const chatBox = document.querySelector('.chatBox')
+        chatBox.appendChild(paragraphElement)
+    });
+}  
+initializeChatBox()
 const socket = io('http://localhost:3000');
 socket.on('connect' , data => {
     const sendBtn = document.getElementById('sendBtn')
@@ -10,8 +23,14 @@ socket.on('connect' , data => {
     })
 })
 socket.on('serverMessage' ,message => {
+    let localStorageValue = localStorage.getItem('messages') ? localStorage.getItem('messages') + '#' + message : message
+    localStorage.setItem('messages' , localStorageValue)
     const paragraphElement = document.createElement('p')
     paragraphElement.innerText = message;
     const chatBox = document.querySelector('.chatBox')
     chatBox.appendChild(paragraphElement)
 })
+// let count = 0;
+// setInterval(() =>{
+//     socket.volatile.emit('ping' , ++count)
+// } , 1000)
